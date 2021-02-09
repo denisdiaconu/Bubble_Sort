234a3bfe125250f4c3854263763e35523d95e607
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-09 16:38:40 +0100

    fixed linters

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -12,6 +12,6 @@ def bubble_sort_by(arr)
   print(arr)
 end
 
-bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
